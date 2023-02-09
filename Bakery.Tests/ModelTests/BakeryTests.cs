718efb65a1eaa79a164fc0f52dd8c1403ae4225d
Chloe ONeil <chloesporri@yahoo.com>
2023-02-09 14:51:51 -0800

    WIP

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -9,8 +9,16 @@ namespace Bakery.Tests
     [TestMethod]
       public void BakeryConstructor_CreatesInstanceOfBakery_Bakery()
       {
-        BakeryItems newBakeryItems = new BakeryItems();
+        BakeryItems newBakeryItems = new BakeryItems(1);
         Assert.AreEqual(typeof(BakeryItems), newBakeryItems.GetType());
+      }
+      [TestMethod]
+      public void GetBakeryType_ReturnsBread_Bread()
+      {
+        int bread = 1;
+        BakeryItems newItem = new BakeryItems(1);
+        string result = newItem;
+        Assert.AreEqual(result, bread);
       }
     }
   }
