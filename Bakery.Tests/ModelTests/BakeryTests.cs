876ce8e92678b78e8ef4c7681b8023a339e1b81d
Chloe ONeil <chloesporri@yahoo.com>
2023-02-09 14:20:06 -0800

    pass first test

--- Bakery.Tests/ModelTests/BakeryTests.cs
+++ Bakery.Tests/ModelTests/BakeryTests.cs
@@ -6,6 +6,11 @@ namespace Bakery.Tests
   [TestClass]
   public class BakeryTests
   {
-    
+    [TestMethod]
+      public void BakeryConstructor_CreatesInstanceOfBakery_Bakery()
+      {
+        BakeryItems newBakeryItems = new BakeryItems();
+        Assert.AreEqual(typeof(BakeryItems), newBakeryItems.GetType());
+      }
+    }
   }
-}
