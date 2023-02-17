e4f00f3d64b5ac2c76cc301967ddbda94009164f
Chloe ONeil <chloesporri@yahoo.com>
2023-02-17 14:26:37 -0800

    refactor Program code to include total price

--- Bakery/Program.cs
+++ Bakery/Program.cs
@@ -9,44 +9,35 @@ namespace Bakery.Models
   {
     public static void Main()
     {
-      Console.WriteLine("Welcome to Pierre's Bakery!");
-      Console.WriteLine("Would you like to buy some bread or pastries? ['B'] for bread or ['P'] for pastries");
+      Bread b = new Bread (5, 2, 1);
+      Pastry p = new Pastry (2, 3, 1);
+      bool done = false;
+      int pQuantity = 0;
+      int bQuantity = 0;
+      while(!done) {
+        Console.WriteLine("Welcome to Pierre's Bakery!");
+        Console.WriteLine("Would you like to buy some bread or pastries? ['P'] for pastries, ['B'] for bread or ['F'] for finished");
       string answer = Console.ReadLine();
-        if (answer == "B" || answer == "b")
+        if (answer == "p" || answer == "P") 
         {
-          Console.WriteLine("How many breads would you like? They are $5 each or 'Buy 2, get 1 Free'");
-          string stringBQuantity = Console.ReadLine();
-          int bQuantity = int.Parse(stringBQuantity);
-          {
-            Bread b = new Bread (5, 2, 1);
-            Console.WriteLine("Your bread will cost $" + b.GetCostForQuantity(bQuantity));
-          }
-        }
-          else if (answer == "P" || answer == "p")
-        {
-          Console.WriteLine("How many pastries would you like? They are $2 each or 'Buy 3, get 1 Free'");
+          Console.WriteLine("How many pastries would you like? They are $2 each or 'Buy 3, get 1 FREE'");
           string stringPQuantity = Console.ReadLine();
-          int pQuantity = int.Parse(stringPQuantity);
-          {
-            Pastry p = new Pastry (2, 3, 1);
-            Console.WriteLine("Your pastries will cost $" + p.GetCostForQuantity(pQuantity));
-          }
-        }
-        else
+          pQuantity = int.Parse(stringPQuantity);
+          Console.WriteLine("Your pastries will cost $" + p.GetCostForQuantity(pQuantity));
+        } else if (answer == "b" || answer == "B") 
         {
-          Console.WriteLine("Are you finished shopping? ['Y' for yes, 'Enter' for no]");
-          string finished = Console.ReadLine();
-          if (finished == "Y" || finished == "y")
-          {
-            Console.WriteLine("Thanks for shopping with us!");
-          }
-          else
-          {
-            Main();
-          }
+          Console.WriteLine("How many breads would you like? They are $5 each or 'Buy 2, get 1 FREE'");
+          string stringBQuantity = Console.ReadLine();
+          bQuantity = int.Parse(stringBQuantity);
+          Console.WriteLine("Your bread will cost $" + b.GetCostForQuantity(bQuantity));
+        } else if (answer == "f" || answer == "F") 
+        {
+          Console.WriteLine("Here's your cost for bread $" + b.GetCostForQuantity(bQuantity) + " and your cost for pastries $" + p.GetCostForQuantity(pQuantity) + ".");
+          Console.WriteLine("Making your total $" + (b.GetCostForQuantity(bQuantity) + p.GetCostForQuantity(pQuantity)) + ". Thank you for shopping at Pierre's!");
+          done=true;
         }
       }
     }
   }
-
+}
 
